e6395ae4443028fece509e5084bdbbc9ab2ae2e6
Tavish Naruka <tavishnaruka@gmail.com>
2017-11-20 16:35:42 +0530

    set a reset pin if RESET_PIN is defined in variant.h

--- cores/nRF5/main.cpp
+++ cores/nRF5/main.cpp
@@ -16,10 +16,30 @@
 #define ARDUINO_MAIN
 #include "Arduino.h"
 
+#if defined(RESET_PIN)
+#include "nrf.h"
+#endif
+
 // Weak empty variant initialization function.
 // May be redefined by variant files.
 void initVariant() __attribute__((weak));
-void initVariant() { }
+void initVariant()
+{
+  #if defined(RESET_PIN)
+  if (((NRF_UICR->PSELRESET[0] & UICR_PSELRESET_CONNECT_Msk) != (UICR_PSELRESET_CONNECT_Connected << UICR_PSELRESET_CONNECT_Pos)) ||
+      ((NRF_UICR->PSELRESET[1] & UICR_PSELRESET_CONNECT_Msk) != (UICR_PSELRESET_CONNECT_Connected << UICR_PSELRESET_CONNECT_Pos))){
+      NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen << NVMC_CONFIG_WEN_Pos;
+      while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+      NRF_UICR->PSELRESET[0] = RESET_PIN;
+      while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+      NRF_UICR->PSELRESET[1] = RESET_PIN;
+      while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+      NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren << NVMC_CONFIG_WEN_Pos;
+      while (NRF_NVMC->READY == NVMC_READY_READY_Busy){}
+      NVIC_SystemReset();
+  }
+  #endif
+}
 
 /*
  * \brief Main entry point of Arduino application
